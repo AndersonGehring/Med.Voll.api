64296a4c21bf4564fdc9e36d7cdc4ef1174f4b21
Anderson Gehring <andersondrive15@gmail.com>
2023-11-09 16:51:37 -0300

    Validação com Bean validation

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,24 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank                //Bean validation o atributo n pode ser nullo nem vazio
+        String nome,
+        @NotBlank
+        @Email                   //confirma se é um email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")      //expressão regular pra ter de 4 a 6 digitor na string crm
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid                     // para validar os objetos que estão dentro do DadosEndereco
+        DadosEndereco endereco) {
 }
