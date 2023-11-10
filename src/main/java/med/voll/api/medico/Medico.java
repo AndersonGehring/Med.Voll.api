d2117cdb43ce98695272106732ac0d0e6d898a71
Anderson Gehring <andersondrive15@gmail.com>
2023-11-10 17:27:54 -0300

    Requisições put e atualizando dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -40,6 +40,10 @@ public class Medico {
         return crm;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public Especialidade getEspecialidade() {
         return especialidade;
     }
@@ -51,5 +55,17 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco = new Endereco(dados.endereco());
+        }
     }
 }
